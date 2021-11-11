798ab9843463aa43900cb252eea6a3eb94a2fe23
JustKahdri <joaco.rm512@gmail.com>
2021-11-11 20:22:36 -0300

    new hero for home page

---
+++ components/ArrowAnimated.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import styles from "@styles/ArrowAnimated.module.css";
+
+const ArrowAnimated = () => (
+  <svg id={styles.arrows}>
+    <polygon className={styles.top} points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 " />
+    <polygon
+      className={styles.middle}
+      points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "
+    />
+    <polygon
+      className={styles.bottom}
+      points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "
+    />
+  </svg>
+);
+
+export default ArrowAnimated;
